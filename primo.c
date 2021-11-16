5bc89f36eca55c603ccf41f79750018762a46aeb
Enrico192002 <55252934+giulioon@users.noreply.github.com>
2021-11-16 15:30:24 +0100

    Update primo.c

--- primo.c
+++ primo.c
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[]) {
 int anni;
 printf("FORSE...(NUMERO PARAMETRI: %D) %s\n", argc, argv[0]);
-if (argc<1) printf("primo argomento utente: %s\n", argv[1]);
-if(argc<2)  {
+if (argc>1) printf("primo argomento utente: %s\n", argv[1]);
+if(argc>2)  {
 anni = atoi(argv[2]);
 if (anni  !=  0)
 printf("secondo argomento utente (%s) valido %d\n",  argv[2], anni);
